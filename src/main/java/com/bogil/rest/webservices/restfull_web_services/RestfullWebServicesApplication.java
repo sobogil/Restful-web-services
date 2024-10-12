65c1662362034c94dbf875840f2f3345b0138f14
sobogil <69839941+sobogil@users.noreply.github.com>
2024-10-12 16:28:14 +0900

    first commit

---
+++ src/main/java/com/bogil/rest/webservices/restfull_web_services/RestfullWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.bogil.rest.webservices.restfull_web_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfullWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfullWebServicesApplication.class, args);
+	}
+
+}

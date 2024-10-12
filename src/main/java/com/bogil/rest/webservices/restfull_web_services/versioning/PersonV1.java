4503d7929077d6a97efc9d8d8fc3c6f7c341a1aa
sobogil <69839941+sobogil@users.noreply.github.com>
2024-10-12 22:36:16 +0900

    first name, second name versioning하기

---
+++ src/main/java/com/bogil/rest/webservices/restfull_web_services/versioning/PersonV1.java
@@ -0,0 +1,27 @@
+package com.bogil.rest.webservices.restfull_web_services.versioning;
+
+public class PersonV1 {
+    public String name;
+
+    public PersonV1() {
+    }
+
+    public PersonV1(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "PersonV1{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
